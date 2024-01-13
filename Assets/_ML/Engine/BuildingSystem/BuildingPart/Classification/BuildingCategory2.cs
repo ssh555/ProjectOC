8cd4065394fda004b79e3b2212196c0db5b170f9
SnowDeer0447 <99851085+SnowDeer0447@users.noreply.github.com>
2024-01-14 00:55:49 +0800

    生产节点类目和预制体配置完成
    接下来配置交互建筑

--- Assets/_ML/Engine/BuildingSystem/BuildingPart/Classification/BuildingCategory2.cs
+++ Assets/_ML/Engine/BuildingSystem/BuildingPart/Classification/BuildingCategory2.cs
@@ -7,27 +7,63 @@ namespace ML.Engine.BuildingSystem.BuildingPart
 	{
 		[LabelText("None")]
 		None = -1,
+		[LabelText("露水收集器")]
+		WaterCollector = 0,
+		[LabelText("水源纯化器")]
+		WaterPurifier = 001,
+		[LabelText("种籽箱")]
+		SeedBox = 002,
+		[LabelText("光流整束器")]
+		Integrator = 003,
+		[LabelText("苗圃")]
+		SeedPlot = 004,
+		[LabelText("捕捞站")]
+		Detector = 005,
+		[LabelText("精炼炉")]
+		Refine = 006,
+		[LabelText("锯木床")]
+		Sawn = 007,
+		[LabelText("纺织机")]
+		Textile = 008,
+		[LabelText("聚合器")]
+		Aggregator = 009,
+		[LabelText("精密加工机")]
+		Processor = 010,
+		[LabelText("风味工坊")]
+		Kitchen = 011,
+		[LabelText("演算仪")]
+		Calculus = 012,
+		[LabelText("仓库")]
+		Store = 100,
+		[LabelText("储液罐")]
+		Reservoir = 101,
+		[LabelText("恒温仓")]
+		Incubator = 102,
+		[LabelText("共鸣轮")]
+		EchoWheel = 103,
+		[LabelText("导流节点")]
+		DiversionNode = 104,
+		[LabelText("生命导流桩")]
+		LifeDiversion = 105,
+		[LabelText("生态投影装置")]
+		Projector = 106,
 		[LabelText("地基")]
-		Foundation = 0,
-		[LabelText("地板")]
-		Floor = 001,
+		Foundation = 200,
 		[LabelText("地基外沿")]
-		FoundationEdge = 002,
+		FoundationEdge = 201,
+		[LabelText("地板")]
+		Floor = 202,
 		[LabelText("墙")]
-		Wall = 003,
+		Wall = 203,
 		[LabelText("柱")]
-		Pillar = 004,
+		Pillar = 204,
 		[LabelText("梁")]
-		Beam = 005,
+		Beam = 205,
 		[LabelText("屋顶")]
-		Roof = 006,
+		Roof = 206,
 		[LabelText("门")]
-		Door = 007,
+		Door = 207,
 		[LabelText("窗")]
-		Window = 008,
-		[LabelText("吊灯")]
-		CeilingLamp = 100,
-		[LabelText("椅子")]
-		Chair = 101,
+		Window = 208,
 	}
 }
