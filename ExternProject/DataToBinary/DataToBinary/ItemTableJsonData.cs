8967e0f02598bd6ead7dccca6750d17bcd825dc7
ssh555 <89600078+ssh555@users.noreply.github.com>
2024-01-15 22:09:32 +0800

    同步sin和csproj

--- ExternProject/DataToBinary/DataToBinary/ItemTableJsonData.cs
+++ ExternProject/DataToBinary/DataToBinary/ItemTableJsonData.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 命名空间
 namespace ML.Engine.InventorySystem
 {
+    // 如果是类内部的类型，需要用类包裹
     [System.Serializable]
     public enum ItemType
     {
@@ -15,13 +17,17 @@ namespace ML.Engine.InventorySystem
         Material,
         Mission,
     }
-
+    
+    // 可序列化
     [System.Serializable]
+    // 结构体|类 名称
     public struct ItemTableJsonData
     {
+        // 属性名称和类型
         public string id;
         public int sort;
         public ItemType itemtype;
+        // 如果有其他类型引用，其他类型也必须一模一样
         public ML.Engine.TextContent.TextContent name;
         public int weight;
         public string icon;
