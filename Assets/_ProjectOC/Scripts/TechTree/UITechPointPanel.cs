1cbe19d9e578c85f27ec50f70a22cc2ee0fca107
pluto156 <1561918156@qq.com>
2024-04-07 14:02:55 +0800

    fix:修复科技树

--- Assets/_ProjectOC/Scripts/TechTree/UITechPointPanel.cs
+++ Assets/_ProjectOC/Scripts/TechTree/UITechPointPanel.cs
@@ -421,7 +421,53 @@ namespace ProjectOC.TechTree.UI
 
         private void ClearTemp()
         {
-            
+            // sprite
+            foreach (var s in tempSprite)
+            {
+                ML.Engine.Manager.GameManager.DestroyObj(s);
+            }
+            tempSprite.Clear();
+            // TPCategory
+            foreach (var s in TPCGO.Values)
+            {
+                ML.Engine.Manager.GameManager.DestroyObj(s);
+            }
+            TPCGO.Clear();
+            // TechTree
+            foreach (var s in TTTPGO.Values)
+            {
+                ML.Engine.Manager.GameManager.DestroyObj(s);
+            }
+            TTTPGO.Clear();
+            foreach (var s in TTTPEGO.Values)
+            {
+                ML.Engine.Manager.GameManager.DestroyObj(s);
+            }
+            TTTPEGO.Clear();
+            // TechPoint
+            foreach (var s in TPCItemGO.Values)
+            {
+                ML.Engine.Manager.GameManager.DestroyObj(s);
+            }
+            TPCItemGO.Clear();
+            foreach (var s in TPUnlockGO.Values)
+            {
+                ML.Engine.Manager.GameManager.DestroyObj(s);
+            }
+            TPUnlockGO.Clear();
+
+            // Temp
+            foreach (var s in tempGO)
+            {
+                ML.Engine.Manager.GameManager.DestroyObj(s);
+            }
+            tempGO.Clear();
+            foreach (var s in tempTimer)
+            {
+                s.OnUpdateEvent = null;
+                s.OnEndEvent -= Refresh;
+            }
+            tempTimer.Clear();
         }
 
         public override void Refresh()
