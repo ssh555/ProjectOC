6bf551a6401a706b10c57226548ffec5d17d23ad
AshStar-2021 <3493329946@qq.com>
2024-04-09 23:30:03 +0800

    捏脸存档3

--- Assets/_ProjectOC/Scripts/PinchFace/CharacterModelPinch.cs
+++ Assets/_ProjectOC/Scripts/PinchFace/CharacterModelPinch.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using AmazingAssets.TerrainToMesh;
 using ProjectOC.PinchFace;
+using Sirenix.OdinInspector;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace ProjectOC.PinchFace
 {
     public class CharacterModelPinch : MonoBehaviour
     {
+        private Transform avatar;
         [SerializeField] 
         List<GameObject> replaceGo = new List<GameObject>();
 
@@ -19,7 +23,7 @@ namespace ProjectOC.PinchFace
 
         public 
         List<GameObject> tempTail = new List<GameObject>();
-
+        [ShowInInspector]
         private Dictionary<string, Transform> boneCatalog = new Dictionary<string, Transform>();
         #region 捏脸函数
         public void ChangeType(PinchPartType2 boneType2, int typeIndex)
@@ -42,21 +46,96 @@ namespace ProjectOC.PinchFace
             //删除gameObject Instance
             _PinchGo = GameObject.Instantiate(_PinchGo);
             // sourceClothing衣服 targetAvatar 角色
+            replaceGo[(int)boneType2] = Stitch(_PinchGo, avatar.gameObject);
             
-            
+            //to-do: 更新骨骼解算目录
             Destroy(_PinchGo);
         }
 
         public void UnEquipItem(PinchPartType2 boneType2, GameObject _PinchGo)
         {
-            //先移出Parent.Transform，然后 删除 gameObject 
+            Transform keyBone = boneTransf[(int)boneType2];
+            //去除骨骼 ，移出Parent.Transform，然后 删除 gameObject 
+            DisCatalog( boneCatalog,keyBone);
+            keyBone.SetParent(this.transform.parent);
+            Destroy(keyBone.gameObject);
+            _PinchGo.transform.SetParent(this.transform.parent);
+            Destroy(_PinchGo);
         }
 
-        void EquipBone(PinchPartType2 boneType2)
+        public GameObject Stitch(GameObject sourceClothing, GameObject targetCharacter)
         {
+            GameObject targetClothingBone = null;
+            //if() 需要添加骨骼
+            //追加骨骼，存入字典  ,S、T 来区分source、target
+            //映射骨骼
+            string keyStr = "Key_";
+            Transform SkeyBone = BreadthFirstSearchForKey(sourceClothing.transform,keyStr);
+            if (SkeyBone != null)
+            {
+                SkeyBone.name = SkeyBone.name.Replace(keyStr, "");
+                Transform TKeyBone = boneCatalog[SkeyBone.name];
             
-        }
+                // set parents,localPosition
+                foreach (var _trans in SkeyBone)
+                {
+                    
+                }
 
+                
+                Transform SNewBone = SkeyBone.GetChild(0);
+                targetClothingBone =GameObject.Instantiate(SNewBone.gameObject);
+                targetClothingBone.name = SNewBone.name;
+            
+            
+                targetClothingBone.transform.SetParent(TKeyBone);
+                targetClothingBone.transform.localPosition = SNewBone.transform.localPosition;
+                targetClothingBone.transform.localRotation = SNewBone.transform.localRotation;
+                targetClothingBone.transform.localScale = SNewBone.transform.localScale;
+                Catalog(boneCatalog,targetClothingBone.transform);
+            }
+
+            //添加smr组件并赋值、映射骨骼
+            SkinnedMeshRenderer[] smrs = sourceClothing.GetComponentsInChildren<SkinnedMeshRenderer>();
+            foreach (var smr in smrs)
+            {
+                if (targetClothingBone == null)
+                {
+                    targetClothingBone = new GameObject(smr.gameObject.name);
+                    targetClothingBone.transform.SetParent(targetCharacter.transform);
+                }
+                
+                SkinnedMeshRenderer targetRenderer = AddSkinnedMeshRenderer(smr,targetClothingBone);
+                targetRenderer.bones = TranslateTransforms (smr.bones, boneCatalog);
+            }
+            return targetClothingBone;
+        }
+        private SkinnedMeshRenderer AddSkinnedMeshRenderer (SkinnedMeshRenderer source, GameObject parent)
+        {
+            SkinnedMeshRenderer target = parent.AddComponent<SkinnedMeshRenderer> ();
+            target.sharedMesh = source.sharedMesh;
+            target.materials = source.materials;
+            return target;
+        }
+        private Transform[] TranslateTransforms (Transform[] sources, Dictionary<string,Transform> _boneCatalog)
+        {
+            Transform[] targets = new Transform[sources.Length];
+            for (int index = 0; index < sources.Length; index++)
+            {
+                if(sources[index] == null)
+                    continue;
+                Transform transf  = _boneCatalog[sources[index].name];
+
+                if(transf != null)
+                    targets[index] = transf;
+                else
+                {
+                    Debug.Log("can't find: "+ sources [index].name);
+                }
+            }
+            return targets;
+        }
+        
         
         public void ChangeTexture(PinchPartType2 boneType2, int textureIndex)
         {
@@ -95,8 +174,8 @@ namespace ProjectOC.PinchFace
         
             void Awake()
             {
-                Transform _avatar = transform.Find("AnMiXiuBone");
-                Catalog(boneCatalog,_avatar);
+                avatar = transform.Find("AnMiXiuBone");
+                Catalog(boneCatalog,avatar);
             }
             
             //将子物体加入字典
@@ -118,7 +197,48 @@ namespace ProjectOC.PinchFace
                 foreach (Transform child in transform)
                     Catalog (dic,child);
             }
+
+            private void DisCatalog(Dictionary<string,Transform>dic, Transform transform)
+            {
+                if (dic.ContainsKey(transform.name))
+                {
+                    dic.Remove(transform.name);
+                }
+
+                foreach (Transform child in transform)
+                {
+                    DisCatalog(dic,child);
+                }
+            }
             
+
+            // 广度优先搜索KeyBone
+            private Transform BreadthFirstSearchForKey(Transform root,string _str)
+            {
+                Queue<Transform> queue = new Queue<Transform>();
+                queue.Enqueue(root);
+
+                while (queue.Count > 0)
+                {
+                    Transform currentNode = queue.Dequeue();
+                    Transform currentTransform = currentNode.transform;
+
+                    // 检查当前Transform的名字是否以 "Key_" 开头
+                    if (currentTransform.name.StartsWith(_str))
+                    {
+                        return currentTransform;
+                    }
+
+                    // 将当前Transform的子Transform加入队列
+                    foreach (Transform child in currentTransform)
+                    {
+                        queue.Enqueue(child);
+                    }
+                }
+
+                // 如果没找到符合条件的Transform，返回null
+                return null;
+            }
             #endregion
     
         
