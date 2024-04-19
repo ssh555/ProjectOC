2abdb42825e7b7078d90521f5b53289137bee77b
许炜鹏 <xu24.66@outlook.com>
2024-04-19 13:27:46 +0800

    节点及交互建筑建造

--- Assets/_ML/Engine/BuildingSystem/BuildingPart/Classification/BuildingCategory3.cs
+++ Assets/_ML/Engine/BuildingSystem/BuildingPart/Classification/BuildingCategory3.cs
@@ -13,48 +13,48 @@ namespace ML.Engine.BuildingSystem.BuildingPart
 		Triangle1 = 101,
 		[LabelText("四方圆形1")]
 		Quagrant1 = 102,
-		[LabelText("附加1")]
-		Extra1 = 103,
+		[LabelText("长方形1")]
+		LBase1 = 103,
 		[LabelText("正方形2")]
 		Base2 = 200,
 		[LabelText("三角形2")]
 		Triangle2 = 201,
 		[LabelText("四方圆形2")]
 		Quagrant2 = 202,
-		[LabelText("附加2")]
-		Extra2 = 203,
+		[LabelText("长方形2")]
+		LBase2 = 203,
 		[LabelText("正方形3")]
 		Base3 = 300,
 		[LabelText("三角形3")]
 		Triangle3 = 301,
 		[LabelText("四方圆形3")]
 		Quagrant3 = 302,
-		[LabelText("附加3")]
-		Extra3 = 303,
+		[LabelText("长方形3")]
+		LBase3 = 303,
 		[LabelText("正方形4")]
 		Base4 = 400,
 		[LabelText("三角形4")]
 		Triangle4 = 401,
 		[LabelText("四方圆形4")]
 		Quagrant4 = 402,
-		[LabelText("附加4")]
-		Extra4 = 403,
+		[LabelText("长方形4")]
+		LBase4 = 403,
 		[LabelText("正方形5")]
 		Base5 = 500,
 		[LabelText("三角形5")]
 		Triangle5 = 501,
 		[LabelText("四方圆形5")]
 		Quagrant5 = 502,
-		[LabelText("附加5")]
-		Extra5 = 503,
+		[LabelText("长方形5")]
+		LBase5 = 503,
 		[LabelText("正方形6")]
 		Base6 = 600,
 		[LabelText("三角形6")]
 		Triangle6 = 601,
 		[LabelText("四方圆形6")]
 		Quagrant6 = 602,
-		[LabelText("附加6")]
-		Extra6 = 603,
+		[LabelText("长方形6")]
+		LBase6 = 603,
 		[LabelText("单人床")]
 		SingleBed = 700,
 		[LabelText("双人床")]
@@ -97,6 +97,8 @@ namespace ML.Engine.BuildingSystem.BuildingPart
 		Ledge = 741,
 		[LabelText("浴池")]
 		Bath = 750,
+		[LabelText("隔断")]
+		Partition = 742,
 		[LabelText("水池")]
 		Pond = 751,
 		[LabelText("水体箱")]
@@ -115,5 +117,9 @@ namespace ML.Engine.BuildingSystem.BuildingPart
 		Curtain = 771,
 		[LabelText("自然")]
 		Nature = 772,
+		[LabelText("挂壁")]
+		Hang = 773,
+		[LabelText("坠饰")]
+		Pendant = 774,
 	}
 }
--- Assets/_ML/Engine/BuildingSystem/BuildingSocket/BuildingSocketType.cs
+++ Assets/_ML/Engine/BuildingSystem/BuildingSocket/BuildingSocketType.cs
@@ -13,5 +13,19 @@ namespace ML.Engine.BuildingSystem.BuildingSocket
 		Floor_Triangle_AdjacementCenter,
 		[LabelText("Floor_Quagrant_AdjacementCenter")]
 		Floor_Quagrant_AdjacementCenter,
+		[LabelText("生命导流桩吸附点")]
+		LifeDiversion,
+		[LabelText("功能建筑吸附点")]
+		PronodeAndInteract,
+		[LabelText("地板表层")]
+		Floorsurface,
+		[LabelText("天花板")]
+		Ceiling,
+		[LabelText("墙面")]
+		Wallsurface,
+		[LabelText("地板中心吸附点")]
+		Floor_Base_Center,
+		[LabelText("地板顶点吸附点")]
+		Floor_Base_Vertex,
 	}
 }
