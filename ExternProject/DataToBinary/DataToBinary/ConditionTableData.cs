18b2a8092744255e4fbe20f23c0bf41a6faa07ba
Blossomone <1767487770@qq.com>
2024-04-27 16:09:54 +0800

    update: 导表

---
+++ ExternProject/DataToBinary/DataToBinary/ConditionTableData.cs
@@ -0,0 +1,36 @@
+﻿using ExcelToJson;
+using System.Collections.Generic;
+
+namespace ML.Engine.Event
+{
+    [System.Serializable]
+    public struct ConditionTableData : IGenData
+    {
+        public string ID;
+        public TextContent.TextContent Name;
+        public CheckType CheckType;
+        public List<string> Param1;
+        public List<int> Param2;
+        public List<float> Param3;
+        public bool GenData(string[] row)
+        {
+            if (string.IsNullOrEmpty(row[0]))
+            {
+                return false;
+            }
+            // 0 -> ID
+            this.ID = Program.ParseString(row[0]);
+            // 1 -> Name
+            this.Name = Program.ParseTextContent(row[1]);
+            // 2 -> CheckType
+            this.CheckType = Program.ParseEnum<CheckType>(row[2]);
+            // 3 -> Param1
+            this.Param1 = Program.ParseStringList(row[3]);
+            // 4 -> Param2
+            this.Param2 = Program.ParseIntList(row[4]);
+            // 5 -> Param3
+            this.Param3 = Program.ParseFloatList(row[5]);
+            return true;
+        }
+    }
+}
