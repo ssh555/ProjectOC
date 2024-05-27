0ffd625e73d9741a295b15fafb986285adc397e0
Blossomone <1767487770@qq.com>
2024-05-27 23:13:38 +0800

    fix mine

--- Assets/_ProjectOC/Scripts/ProNode/UI/UIMineProNode.cs
+++ Assets/_ProjectOC/Scripts/ProNode/UI/UIMineProNode.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 using System.Linq;
 using ML.Engine.Utility;
 using static ProjectOC.ProNodeNS.UI.UIMineProNode;
-using ML.Engine.Manager;
 
 namespace ProjectOC.ProNodeNS.UI
 {
@@ -308,7 +307,7 @@ namespace ProjectOC.ProNodeNS.UI
                 }
                 else if(CurProNodeMode == ProNodeSelectMode.Mine)
                 {
-                    GameManager.Instance.ABResourceManager.InstantiateAsync("Prefab_Mine_UIPanel/Prefab_Mine_UI_SelectMineralSourcesPanel.prefab").Completed += (handle) =>
+                    ML.Engine.Manager.GameManager.Instance.ABResourceManager.InstantiateAsync("Prefab_Mine_UIPanel/Prefab_Mine_UI_SelectMineralSourcesPanel.prefab").Completed += (handle) =>
                     {
                         SelectMineralSourcesPanel selectMineralSourcesPanel = handle.Result.GetComponent<SelectMineralSourcesPanel>();
                         selectMineralSourcesPanel.transform.SetParent(ML.Engine.Manager.GameManager.Instance.UIManager.GetCanvas.transform, false);
@@ -412,7 +411,7 @@ namespace ProjectOC.ProNodeNS.UI
                 bool hasMine = ProNode.HasMine;
                 ProNode_Mine.Find("Icon").gameObject.SetActive(!hasMine);
                 ProNode_Mine.Find("IconMine").gameObject.SetActive(hasMine);
-                ProNode_Mine.Find("Name").gameObject.SetActive(hasMine);
+                ProNode_Mine.Find("Name").gameObject.SetActive(hasMine && ProNode.IsOnProduce);
                 ProNode_Mine.Find("Name").GetComponent<TMPro.TextMeshProUGUI>().text = PanelTextContent.textChangeMine;
                 #endregion
 
@@ -423,7 +422,7 @@ namespace ProjectOC.ProNodeNS.UI
                     Transform mine = ProductBtnList.GetBtn(i).transform;
                     string productID = ProNode.DataContainer.GetID(i);
                     int stackAll = ProNode.DataContainer.GetAmount(i, DataNS.DataOpType.StorageAll);
-                    int stackMax = ProNode.MineStackMax * ProNode.MineDatas[i].GainNum;
+                    int stackMax = ProNode.MineStackMax * ProNode.MineDatas[i].GainItems.num;
                     if (!tempSprite.ContainsKey(productID))
                     {
                         tempSprite[productID] = ML.Engine.InventorySystem.ItemManager.Instance.GetItemSprite(productID);
@@ -642,13 +641,14 @@ namespace ProjectOC.ProNodeNS.UI
             {
                 ProNode_Mine.Find("Mask").GetComponent<Image>().fillAmount = 0;
             }
+            ProNode_Mine.Find("Name").gameObject.SetActive(ProNode.HasMine && ProNode.IsOnProduce);
             int mineCnt = ProNode.MineDatas?.Count ?? 0;
             for (int i = 0; i < mineCnt; i++)
             {
                 Transform mine = ProductBtnList.GetBtn(i).transform;
                 string productID = ProNode.DataContainer.GetID(i);
                 int stackAll = ProNode.DataContainer.GetAmount(i, DataNS.DataOpType.StorageAll);
-                int stackMax = ProNode.MineStackMax * ProNode.MineDatas[i].GainNum;
+                int stackMax = ProNode.MineStackMax * ProNode.MineDatas[i].GainItems.num;
                 if (!tempSprite.ContainsKey(productID))
                 {
                     tempSprite[productID] = ML.Engine.InventorySystem.ItemManager.Instance.GetItemSprite(productID);
