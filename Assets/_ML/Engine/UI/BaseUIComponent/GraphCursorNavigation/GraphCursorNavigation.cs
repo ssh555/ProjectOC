b4a67682fa83e9698bfc75af104dfda4f9c7223b
pluto156 <1561918156@qq.com>
2024-06-03 11:13:59 +0800

    update:采矿

--- Assets/_ML/Engine/UI/BaseUIComponent/GraphCursorNavigation/GraphCursorNavigation.cs
+++ Assets/_ML/Engine/UI/BaseUIComponent/GraphCursorNavigation/GraphCursorNavigation.cs
@@ -24,6 +24,7 @@ namespace ML.Engine.UI
         /// <summary>
         /// Center 在Content 坐标系下的坐标
         /// </summary>
+        [ShowInInspector]
         public Vector3 CenterPos { get {
 
                 Vector2 localPosition;
@@ -437,6 +438,49 @@ namespace ML.Engine.UI
             this.ScrollRect.content.position = this.center.position + Offset;
             OnScaleChanged?.Invoke();
         }
+
+        /// <summary>
+        /// 移动到指定位置 传入指定位置在Content坐标系下的anchoredPosition
+        /// </summary>
+        public void MoveCenterToPos(Vector2 targetPos)
+        {
+            //判断是否在内框
+
+            RectTransform ViewPort = this.content.parent as RectTransform;
+            RectTransform Content = this.content as RectTransform;
+            Vector2 ViewPortSize = ViewPort.rect.size;
+            Vector2 ContentSize = Content.rect.size;
+
+            bool isTouchDown = Vector2.Distance(targetPos, new Vector2(0, -(ContentSize.y - ViewPortSize.y) / 2)) < 0.1;
+            bool isTouchUP = Vector2.Distance(targetPos, new Vector2(0, (ContentSize.y - ViewPortSize.y) / 2)) < 0.1;
+            bool isTouchLeft = Vector2.Distance(targetPos, new Vector2(-(ContentSize.x - ViewPortSize.x) / 2,0)) < 0.1;
+            bool isTouchRight = Vector2.Distance(targetPos, new Vector2((ContentSize.x - ViewPortSize.x) / 2,0)) < 0.1;
+
+            if (isTouchDown || isTouchUP || isTouchLeft|| isTouchRight) 
+            {
+                //调整Center的pos 使 Center在Content坐标系下的坐标为TargetPos
+
+                //计算targetPos在 Center坐标系下的坐标
+
+                //计算targetPos的世界坐标
+                var WorldPos = Content.TransformPoint(targetPos);
+                Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(null, WorldPos);
+                Vector2 localPosition;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(center, screenPoint, null, out localPosition);
+                Debug.Log("localPosition " + localPosition);
+                center.anchoredPosition = localPosition;
+            }
+            else
+            {
+                //在内框 只需改变Content的位置与Center重合
+                Debug.Log("Content.position " + Content.TransformPoint(targetPos));
+                Content.position = Content.TransformPoint(targetPos);
+                Content.anchoredPosition *= -1;
+            }
+        }
+
+
+
         public void EnableGraphCursorNavigation(InputAction NavigationInputAction, InputAction ZoomInInputAction = null, InputAction ZoomOutInputAction = null)
         {
             this.BindNavigationInput(NavigationInputAction);
